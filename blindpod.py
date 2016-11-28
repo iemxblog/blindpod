1cb9b56cc3b01fccf40252795adb0c2f534c59a0
iemxblog <iemxblog@gmail.com>
2016-11-28 15:21:54 +0100

    Added code for keyboard management with evdev

--- blindpod.py
+++ blindpod.py
@@ -1,5 +1,6 @@
 import feedparser
 import urllib.request
+import evdev
 
 subscriptions = [
     'http://radiofrance-podcast.net/podcast09/rss_11549.xml' # Sur les épaules de Darwin
@@ -38,4 +39,23 @@ def download(n, i):
     print("File downloaded.")
 
 
-download(1, 4)
+#download(1, 4)
+
+devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+for device in devices:
+    print(device.fn, device.name, device.phys)
+
+device = evdev.InputDevice('/dev/input/event0')
+print(device)
+
+print(device.leds(verbose=True))
+
+for event in device.read_loop():
+    if event.type == evdev.ecodes.EV_KEY:
+        print(evdev.categorize(event))
+        print("type")
+        print(event.type)
+        print("code")
+        print(event.code)
+        print("value")
+        print(event.value)
