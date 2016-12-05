9995d442666d112f446e8307db0b6166fefe53c6
iemxblog <iemxblog@gmail.com>
2016-12-05 11:29:44 +0100

    Added a stack interface

--- blindpod.py
+++ blindpod.py
@@ -1,13 +1,9 @@
 import evdev
 import feed
-
-subscriptions = [
-    'http://radiofrance-podcast.net/podcast09/rss_11549.xml' # Sur les épaules de Darwin
-    , 'http://radiofrance-podcast.net/podcast09/rss_13942.xml' # Si tu écoutes j'annule tout
-    , 'http://feeds.feedburner.com/PodcastScience' # Podcast science
-]
+import stack
 
 
+'''
 print("Updating database")
 feeds = feed.getFeeds(subscriptions)
 print("Database updated.")
@@ -21,6 +17,8 @@ print(feed.duration(feeds, 1, 4))
 #feed.download(feeds, 1, 4)
 
 '''
+
+'''
 devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
 for device in devices:
     print(device.fn, device.name, device.phys)
@@ -32,12 +30,14 @@ device = evdev.InputDevice('/dev/input/event0')
 #print(device.leds(verbose=True))
 '''
 
-num_keycodes = {82:0, 79:1, 80:2, 81:3, 75:4, 76:5, 77:6, 71:7, 72:8, 73:9}
+kc = {82:stack.KEY_0, 79:stack.KEY_1, 80:stack.KEY_2, 81:stack.KEY_3, 75:stack.KEY_4, 76:stack.KEY_5, 77:stack.KEY_6, 71:stack.KEY_7, 72:stack.KEY_8, 73:stack.KEY_9,
+    83:stack.KEY_DOT, 96:stack.KEY_ENTER, 78:stack.KEY_PLUS, 74:stack.KEY_MINUS, 55:stack.KEY_MULT, 98:stack.KEY_DIV, 69:stack.KEY_NUMLOCK}
 
 for event in device.read_loop():
     if event.type == evdev.ecodes.EV_KEY:
-        if event.code in num_keycodes.keys():
-            feed.download(feeds, num_keycodes[event.code], 0)
+        if event.code in kc.keys():
+            stack.key(kc[event.code])
+'''
         print(evdev.categorize(event))
         print("type")
         print(event.type)
@@ -45,3 +45,4 @@ for event in device.read_loop():
         print(event.code)
         print("value")
         print(event.value)
+'''
