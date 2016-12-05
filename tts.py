5bc716a6d6a6a0dce02cc94f9673859fb81f8a4e
iemxblog <iemxblog@gmail.com>
2016-12-05 21:57:50 +0100

    Pause MPD when TTS is active

--- tts.py
+++ tts.py
@@ -1,6 +1,13 @@
 from espeak import espeak
-
+from mpd import MPDClient
 def say(msg):
+    client = MPDClient()
+    client.connect("localhost", 6600)
+    s = client.status()['state']
+    if s == 'play':
+        client.pause()
+    client.close()
+    client.disconnect()
     print(msg)
     espeak.set_voice("fr")
     espeak.synth(msg)
