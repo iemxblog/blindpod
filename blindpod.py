d8b010d56e37554054bc2cdbc8c95ab7612c7d0b
iemxblog <iemxblog@gmail.com>
2016-12-05 21:42:56 +0100

    Deleted useless code

--- blindpod.py
+++ blindpod.py
@@ -3,32 +3,7 @@ import feed
 import fsm
 
 
-'''
-print("Updating database")
-feeds = feed.getFeeds(subscriptions)
-print("Database updated.")
-
-print(feed.feedTitle(feeds, 1))
-print(feed.feedSubTitle(feeds, 1))
-print(feed.episodeTitle(feeds, 1, 4))
-print(feed.episodeSubTitle(feeds, 1, 4))
-print(feed.duration(feeds, 1, 4))
-
-#feed.download(feeds, 1, 4)
-
-'''
-
-'''
-devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
-for device in devices:
-    print(device.fn, device.name, device.phys)
-'''
 device = evdev.InputDevice('/dev/input/event0')
-
-'''
-# print(device)
-#print(device.leds(verbose=True))
-'''
 
 kc = {82:fsm.KEY_0, 79:fsm.KEY_1, 80:fsm.KEY_2, 81:fsm.KEY_3, 75:fsm.KEY_4, 76:fsm.KEY_5, 77:fsm.KEY_6, 71:fsm.KEY_7, 72:fsm.KEY_8, 73:fsm.KEY_9,
     83:fsm.KEY_DOT, 96:fsm.KEY_ENTER, 78:fsm.KEY_PLUS, 74:fsm.KEY_MINUS, 55:fsm.KEY_MULT, 98:fsm.KEY_DIV, 69:fsm.KEY_NUMLOCK}
@@ -37,12 +12,3 @@ for event in device.read_loop():
     if event.type == evdev.ecodes.EV_KEY:
         if event.code in kc.keys() and event.value == 0:
             fsm.key(kc[event.code])
-'''
-        print(evdev.categorize(event))
-        print("type")
-        print(event.type)
-        print("code")
-        print(event.code)
-        print("value")
-        print(event.value)
-'''
